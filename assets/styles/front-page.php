a0776976b43bc69ff6985a7fb50a22dd707fdd4a
Artem Gorobec <gorodecartem@gmail.com>
2016-12-22 12:37:38 +0200

    22/12 12.20

---
+++ assets/styles/front-page.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * Template Name: front_page
+ */
+?>
+    <div class="container-fluid" id="banner-container">
+        <div class="container home-container">
+            <div class="row banner-content">
+                <div class="col-sm-12 col-xs-12">
+                    <h1>
+                        <span class="text1">Sydney's</span><br>
+                        <span class="text2">craigs courier</span>
+                    </h1>
+                    <h5>
+                        We can deliver your parcel within a 60km radius of Sydney<br>
+                    within 3hrs. Simple, Fast & Reliable. Oh and yes you can book<br>
+                    today and we'll deliver today! ;)
+                    </h5>
+                    <div class="btn-group">
+                        <a class="btn btn-primary btn-lg" href="#">Instant Quote</a>
+                        <a class="btn btn-primary btn-lg" href="#">Book Now</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="section-2">
+        <div class="container">
+            <div class="col-lg-5 col-md-4 col-sm-5 col-xs-12">
+                <img src="http://nev.local/wp-content/uploads/2016/11/banner_03.png" alt=""
+                     class="img-responsive">
+            </div>
+            <div class="text col-lg-7 col-md-8 col-sm-7 col-xs-12">
+                <h3><span class="red">Sydney's</span> Express Courier</h3><br>
+                <div class="text1">
+                    <p>The courier industry has typically been terrible. Think about every<br>
+                        courier experience you have had. You never know who is delivering<br>
+                        your goods, or when they will be delivered. Same Day Express<br>
+                        changes this.
+                    </p>
+                </div>
+                <div class="text2">
+                    <p>When you place your booking with Same Day Express it uses the technology<br>
+                        from Zoom2u. The booking will automatically be dispatched to the closest<br>
+                        courier. Once your booking is linked to a courier (usually less than 5mins) you<br>
+                        will receive a notification. You'll receive details on who the courier is, their contact<br>
+                        number (allowing you to call or SMS) and the ability to track their location. You<br>
+                        will no longer need to wonder where your courier is and how long until they arrive.<br>
+                    </p>
+                </div>
+                <div class="text3">
+                    <p>Using Same Day Express will change your experience of working with a courier.<br>
+                    We want you to have an experience that leaves you wondering why all courier<br>
+                    companies don't operate like this. You can <span class="red2">book online</span> or call us on <span class="red2">02 9098 5100</span><br>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="section-3">
+        <div class="container">
+            <div class="row">
+                <a href="">
+                    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 block1">
+                        <img src="http://nev.local/wp-content/uploads/2016/11/img.block1_-1.png">
+                        <h5 class="link-box-title">Fast Courier Service</h5>
+                        <p>We often get calls which go like this 'Can we<br>
+                            book today and get it delivered today?' YES<br>
+                            that's exactly what we do :)</p>
+                    </div>
+                </a>
+                <a href="">
+                    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 block2">
+                        <img src="http://nev.local/wp-content/uploads/2016/11/img.block2_.png">
+                        <h5 class="link-box-title">Sydney Courier</h5>
+                        <p>We can deliver your parcel all around Sydney.<br>
+                            We cover areas as far as Campbelltown,<br>
+                            Parramatta, Hornsby, Northern Beaches,<br>
+                            Eastern Suburbs and many more</p>
+                    </div>
+                </a>
+                <a href="">
+                    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 block3">
+                            <img src="http://nev.local/wp-content/uploads/2016/11/img.block3_.png">
+                            <h5 class="link-box-title">Insurance</h5>
+                            <p>Are you used to courier companies who don't<br>
+                                care about your goods? We care! We care<br>
+                                so much that we self insure your goods<br>
+                                up to the value of $300.</p>
+                </div>
+                </a>
+            </div>
+        </div>
+    </div>
