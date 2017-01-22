7a29fbdc1ce6a6fe1485824af0846c1e3a202b91
Artem Gorobec <gorodecartem@gmail.com>
2017-01-22 23:07:28 +0200

    22/01 19/00

---
+++ templates/about-us-page.php
@@ -0,0 +1,43 @@
+<?php
+/*
+Template Name: about_us_page
+*/
+?>
+
+    <div class="container-fluid" id="about-container">
+        <div class="container about-container">
+                <h1>About us</h1>
+        </div>
+    </div>
+    <div class="container text-container">
+        <div class="text1">
+            <p><a href="">Craigscouriers.com.au</a> was created in September 2010 to become the preferred
+                express courier service for the Canberra and Sydney areas. How have we<br>
+                become the so successful? Craig's Couriers puts our customers first.
+                We believe that everyone deserves fast, friendly service.
+            </p>
+        </div>
+        <div class="text2">
+            <p>Our background was born in the Southern Highlands of New South Wales,
+                Australia and our couriers have driven all varieties of vehicles: cars, vans, buses,<br>
+                trucks, motorbikes. Our professional drivers have experience dealing in nearly all
+                weathers and circumstances, giving you peace of mind while your needs<br>
+                are met.<br>
+            </p>
+        </div>
+        <div class="text3">
+            <p>The courier industry has historically been built around the logistics
+                of large scale transport providers. At Craig's Couriers, we strive to provide the same great<br>
+                price you get from a large provider but with the customer service expected from
+                a neighbourly Australian-run business.
+            </p>
+        </div>
+        <div class="text4">
+            <p>n May 2016, Craig's Couriers decided to join forces with a fellow Australian-owned
+                and friendly courier, Zoom2u. Together with Zoom2u, we are able to better<br>
+                service our customers with faster, more transparent deliveries in all the
+                same areas we've serviced since day dot. Zoom2u was founded by Steve Orenstein<br>
+                in 2014 to change the courier industry in Australia for the better.
+            </p>
+        </div>
+    </div>
