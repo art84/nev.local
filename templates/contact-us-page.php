d7769efc2e05f938105c8fe75cdf1fc452c519ff
Artem Gorobec <gorodecartem@gmail.com>
2017-01-25 00:55:38 +0200

    25/01 01/00

---
+++ templates/contact-us-page.php
@@ -0,0 +1,50 @@
+<?php
+/*
+Template Name: contact_us_page
+*/
+?>
+
+    <div class="container-fluid" id="contact-us-container">
+        <div class="container contact-container">
+                <h1>Contact us</h1>
+        </div>
+    </div>
+    <div class="container info-container">
+        <div class="text1">
+        <p>
+            Feel free to <a href="">contact us</a> to find out more about Craig's Couriers express courier service.
+        </p>
+        </div>
+            <div class="row contact-us">
+                <div class="col-md-3 email-2">
+                    <div class="foto-icons-email">
+                        <img src="http://nev.local/wp-content/uploads/2017/01/contact_email.png">
+                    </div>
+                    <p><span class="info1">Email</span></p>
+                    <p><span class="info2">hello@zoom2u.com</span></p>
+                </div>
+                <div class="col-md-3 phone">
+                    <div class="foto-icons">
+                        <img src="http://nev.local/wp-content/uploads/2017/01/contact_phone.png">
+                    </div>
+                    <p><span class="info1">Phone</span></p>
+                    <p><span class="info2">+61 2 9098 5100</span></p>
+                </div>
+                <div class="col-md-6 address">
+                    <div class="foto-icons">
+                        <img src="http://nev.local/wp-content/uploads/2017/01/contact_3.png">
+                    </div>
+                    <p><span class="info1">Office Address</span></p>
+                    <p><span class="info2">hSuite 102, 221 Miller St North Sydney NSW, 2060 Australia</span></p>
+                </div>
+            </div>
+            <div class="text2">
+                <p>
+                    If you would like to get in contact with our CEO <span class="red-text">Steve Orenstein</span>
+                    please email <span class="red-text">steve@zoom2u.com</span>
+                </p>
+                <p>
+                    Craig's Couriers is 100% owned by Zoom2u Pty Ltd (ABN: 70 169 869 327)
+                </p>
+            </div>
+    </div>
